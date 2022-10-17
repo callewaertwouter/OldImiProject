a545bf91c499c309216fe0fd22b8d908af8dcb21
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 20:46:23 +0200

    Add dbcontext

---
+++ src/Imi.Project.Api/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Imi.Project.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Imi.Project.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<UnitsOfMeasure> UnitsOfMeasure { get; set; }
+        public DbSet<Ingedrients> Ingedrients { get; set; }
+        public DbSet<Recipes> Recipes { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
