a09c77f6a6ce41cb5958ad4ca9d0a38dd1278a9f
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 19:53:01 +0200

    Create base entity

---
+++ src/Imi.Project.Api/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Imi.Project.Api.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime LastEditedOn { get; set; }
+    }
+}
