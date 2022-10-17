19c00b83e974d11ce9ee71a097078792959e3b6a
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 21:00:56 +0200

    Add seeding data for units of measure

---
+++ src/Imi.Project.Api/Data/Seeding/UnitOfMeasureSeeder.cs
@@ -0,0 +1,19 @@
+﻿using Imi.Project.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Imi.Project.Api.Data.Seeding
+{
+    public class UnitOfMeasureSeeder
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UnitOfMeasure>().HasData(
+                new UnitOfMeasure { MeasureUnit = "gram"},
+                new UnitOfMeasure { MeasureUnit = "liter" },
+                new UnitOfMeasure { MeasureUnit = "blik(ken)" },
+                new UnitOfMeasure { MeasureUnit = "tak(ken)" },
+                new UnitOfMeasure { MeasureUnit = "blaadje(s)" }
+                );
+        }
+    }
+}
