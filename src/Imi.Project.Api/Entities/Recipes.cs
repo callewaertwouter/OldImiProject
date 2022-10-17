9295ff51247d3cc51a028b57ae516673ed9f2a8e
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 19:56:41 +0200

    Add entity for recipes

---
+++ src/Imi.Project.Api/Entities/Recipes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Entities
+{
+    public class Recipes : BaseEntity
+    {
+        [Required]
+        public string Title { get; set; }
+
+        // Props to add: List of ingedriënts, description and which user made it
+    }
+}
