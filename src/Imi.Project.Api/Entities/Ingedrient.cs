a1b9cbd76e8483d14b6e06d3a3642dd138867e02
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 22:02:32 +0200

    Remove incorrectly placed folders

--- src/Imi.Project.Api/Entities/Ingedrient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Imi.Project.Api.Entities
-{
-    public class Ingedrient : BaseEntity
-    {
-        public string Name { get; set; }
-
-        //TODO Add prop for unit of measure (gram, liter, etc.)
-    }
-}
--- src/Imi.Project.Api/Entities/UnitOfMeasure.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Imi.Project.Api.Entities
-{
-    public class UnitOfMeasure
-    {
-        // This class only contains units of measure (gram, liter, etc.) and will not inherit BaseEntity as users will not create units on their own
-
-        public string MeasureUnit { get; set; }
-    }
-}
