b702a91035ff1c16f3400e75c789e86ffa520d2f
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 23:20:42 +0200

    Add ID to units of measure

--- src/Imi.Project.Api.Core/Entities/UnitOfMeasure.cs
+++ src/Imi.Project.Api.Core/Entities/UnitOfMeasure.cs
@@ -1,9 +1,12 @@
-﻿namespace Imi.Project.Api.Core.Entities
+﻿using System;
+
+namespace Imi.Project.Api.Core.Entities
 {
     public class UnitOfMeasure
     {
         // This class only contains units of measure (gram, liter, etc.) and will not inherit BaseEntity as users will not create units on their own
 
+        public Guid Id { get; set; }
         public string MeasureUnit { get; set; }
     }
 }
--- src/Imi.Project.Api.Infrastructure/Data/Seeding/UnitOfMeasureSeeder.cs
+++ src/Imi.Project.Api.Infrastructure/Data/Seeding/UnitOfMeasureSeeder.cs
@@ -1,5 +1,6 @@
 ﻿using Imi.Project.Api.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Imi.Project.Api.Infrastructure.Data.Seeding
 {
@@ -8,16 +9,16 @@ namespace Imi.Project.Api.Infrastructure.Data.Seeding
         public static void Seed(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UnitOfMeasure>().HasData(
-                new UnitOfMeasure { MeasureUnit = "gram" },
-                new UnitOfMeasure { MeasureUnit = "liter" },
-                new UnitOfMeasure { MeasureUnit = "blik(ken)" },
-                new UnitOfMeasure { MeasureUnit = "tak(ken)" },
-                new UnitOfMeasure { MeasureUnit = "blaadje(s)" },
-                new UnitOfMeasure { MeasureUnit = "deciliter" },
-                new UnitOfMeasure { MeasureUnit = "centiliter" },
-                new UnitOfMeasure { MeasureUnit = "bakje(s)" },
-                new UnitOfMeasure { MeasureUnit = "vrij naar keuze" },
-                new UnitOfMeasure { MeasureUnit = "geen" }
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), MeasureUnit = "gram" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000002"), MeasureUnit = "liter" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000003"), MeasureUnit = "blik(ken)" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000004"), MeasureUnit = "tak(ken)" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000005"), MeasureUnit = "blaadje(s)" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000006"), MeasureUnit = "deciliter" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000007"), MeasureUnit = "centiliter" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000008"), MeasureUnit = "bakje(s)" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000009"), MeasureUnit = "vrij naar keuze" },
+                new UnitOfMeasure { Id = Guid.Parse("00000000-0000-0000-0000-000000000010"), MeasureUnit = "geen" }
                 );
         }
     }
