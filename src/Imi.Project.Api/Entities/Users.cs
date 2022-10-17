9570d32077e4e33a0dd2f92d7e5c1aa05371cff0
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 20:16:12 +0200

    Add entity for users

---
+++ src/Imi.Project.Api/Entities/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Entities
+{
+    public class Users : BaseEntity
+    {
+        [Required]
+        public string Email { get; set; }
+
+        // Not setting a password for now since it's just a simple login functionality and only meant to show users keeping created recipes in database.
+    }
+}
