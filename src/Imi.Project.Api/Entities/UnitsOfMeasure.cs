461f63551d2f541d8719b2195ab9dc9f9d983495
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 20:14:38 +0200

    Rename entity

--- src/Imi.Project.Api/Entities/UnitsOfMeasure.cs
+++ src/Imi.Project.Api/Entities/UnitsOfMeasure.cs
@@ -1,6 +1,6 @@
 ﻿namespace Imi.Project.Api.Entities
 {
-    public class UnitOfMeasure
+    public class UnitsOfMeasure
     {
         // This class only contains units of measure (gram, liter, etc.) and will not inherit BaseEntity as users will not create units on their own
 
