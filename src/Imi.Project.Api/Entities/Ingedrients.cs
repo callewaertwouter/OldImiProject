d89a0f65a6649722d37ae710813748e2f5bd3d0a
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-17 20:11:40 +0200

    Add entity for ingedrients

---
+++ src/Imi.Project.Api/Entities/Ingedrients.cs
@@ -0,0 +1,9 @@
+﻿namespace Imi.Project.Api.Entities
+{
+    public class Ingedrients
+    {
+        public string Name { get; set; }
+
+        //TODO: Add prop for unit of measure (gram, liter, etc.)
+    }
+}
