d3442bbf185122c12f81985e07b85469eb770a8d
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-19 21:15:54 +0200

    Add ingedrient repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/IngedrientRepository.cs
@@ -0,0 +1,19 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Infrastructure.Data;
+using Imi.Project.Api.Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class IngedrientRepository : BaseRepository<Ingedrient>, IIngedrientRepository
+    {
+        public IngedrientRepository(AppDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
