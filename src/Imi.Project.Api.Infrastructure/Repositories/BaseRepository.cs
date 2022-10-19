2664d66d2a685ac6b87ba072c10a84dc28aac500
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 00:52:10 +0200

    Add base repository

--- src/Imi.Project.Api.Infrastructure/Repositories/BaseRepository.cs
+++ src/Imi.Project.Api.Infrastructure/Repositories/BaseRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Infrastructure;
 using Imi.Project.Api.Infrastructure.Data;
-using Imi.Project.Api.Infrastructure.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
