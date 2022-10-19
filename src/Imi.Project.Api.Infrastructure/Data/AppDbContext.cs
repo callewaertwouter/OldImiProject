994a8ff618c5548c40a4e5b009b6e6fc2f6ce716
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-19 12:01:57 +0200

    Add linking table fluent api configuration

--- src/Imi.Project.Api.Infrastructure/Data/AppDbContext.cs
+++ src/Imi.Project.Api.Infrastructure/Data/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace Imi.Project.Api.Infrastructure.Data
         public DbSet<Ingedrient> Ingedrients { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<RecipeIngedrient> RecipeIngedrients { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
@@ -25,6 +26,19 @@ namespace Imi.Project.Api.Infrastructure.Data
             IngedrientSeeder.Seed(modelBuilder);
             RecipeSeeder.Seed(modelBuilder);
             UserSeeder.Seed(modelBuilder);
+
+            modelBuilder.Entity<RecipeIngedrient>()
+                .HasKey(ri => new { ri.RecipeId, ri.IngedrientId });
+
+            modelBuilder.Entity<RecipeIngedrient>()
+                .HasOne(ri => ri.Recipe)
+                .WithMany(r => r.ListOfIngedrients)
+                .HasForeignKey(ri => ri.RecipeId);
+
+            modelBuilder.Entity<RecipeIngedrient>()
+                .HasOne(ri => ri.Ingedrient)
+                .WithMany(i => i.UsedInRecipes)
+                .HasForeignKey(ri => ri.IngedrientId);
         }
     }
 }
