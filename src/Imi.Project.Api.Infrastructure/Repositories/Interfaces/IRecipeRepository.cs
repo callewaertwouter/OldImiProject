c6f85c84bcac9c4e915706beacf6ab05ce653204
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-19 21:14:42 +0200

    Add interface for recipe repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories.Interfaces
+{
+    public interface IRecipeRepository : IBaseRepository<Recipe>
+    {
+        Task<IEnumerable<Recipe>> GetByUserIdAsync(Guid id);
+
+        Task<IEnumerable<Recipe>> SearchAsync(string search);
+    }
+}
