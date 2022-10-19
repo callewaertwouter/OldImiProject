bba9824c6380743c367394c51dfedde8100b53d6
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-19 21:14:27 +0200

    Add basic interface repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+
+        Task<IEnumerable<T>> ListAllAsync();
+
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<T> AddAsync(T entity);
+
+        Task<T> DeleteAsync(T entity);
+    }
+}
