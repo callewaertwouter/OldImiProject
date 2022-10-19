8af5d308b7594fb0971227628f2d782b606c9066
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-19 21:15:35 +0200

    Add base repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,68 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Infrastructure.Data;
+using Imi.Project.Api.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _dbContext;
+
+        public BaseRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dbContext.Set<T>().AsQueryable();
+        }
+
+        public virtual async Task<IEnumerable<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().SingleOrDefaultAsync(t => t.Id.Equals(id));
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            entity.LastEditedOn = DateTime.UtcNow;
+
+            _dbContext.Set<T>().Update(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            entity.CreatedOn = DateTime.UtcNow;
+            entity.LastEditedOn = DateTime.UtcNow;
+
+            _dbContext.Set<T>().Add(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<T> DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
