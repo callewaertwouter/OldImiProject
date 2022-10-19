a80c04dc3979366732c3ffb215459598b3429355
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-19 21:15:04 +0200

    Add interface for user repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories.Interfaces
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<IEnumerable<User>> SearchAsync(string search);
+    }
+}
