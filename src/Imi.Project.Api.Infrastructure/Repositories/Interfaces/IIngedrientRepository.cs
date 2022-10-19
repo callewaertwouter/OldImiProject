1d1cb6f21eb2b96d90501e1b1397c7732c35fbc5
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-19 21:14:54 +0200

    Add interface for ingedrient repository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/Interfaces/IIngedrientRepository.cs
@@ -0,0 +1,9 @@
+﻿using Imi.Project.Api.Core.Entities;
+
+namespace Imi.Project.Api.Infrastructure.Repositories.Interfaces
+{
+    public interface IIngedrientRepository : IBaseRepository<Ingedrient>
+    {
+
+    }
+}
