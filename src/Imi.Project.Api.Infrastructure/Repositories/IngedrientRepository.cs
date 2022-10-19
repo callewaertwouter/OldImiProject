27888908045b12e78f60ec7e11f8bb0271374f13
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 00:52:47 +0200

    Update usings

--- src/Imi.Project.Api.Infrastructure/Repositories/IngedrientRepository.cs
+++ src/Imi.Project.Api.Infrastructure/Repositories/IngedrientRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Infrastructure;
 using Imi.Project.Api.Infrastructure.Data;
-using Imi.Project.Api.Infrastructure.Repositories.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Infrastructure.Repositories
 {
