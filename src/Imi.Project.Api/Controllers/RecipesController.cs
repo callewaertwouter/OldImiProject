795ac0774069833af615ba0da0311db55fc63010
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:30:31 +0200

    Add API Controller for recipes

---
+++ src/Imi.Project.Api/Controllers/RecipesController.cs
@@ -0,0 +1,17 @@
+﻿using Imi.Project.Api.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        protected readonly IRecipeRepository _recipeRepository;
+
+        public RecipesController(IRecipeRepository recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+    }
+}
