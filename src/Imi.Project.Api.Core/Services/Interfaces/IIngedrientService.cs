2527c8ca5df4f7f1d226f33df07de031b2ff9698
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:27:49 +0200

    Add interface for ingedrient service

---
+++ src/Imi.Project.Api.Core/Services/Interfaces/IIngedrientService.cs
@@ -0,0 +1,15 @@
+﻿using Imi.Project.Api.Core.Services.Models.IngedrientService;
+using System;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Services.Interfaces
+{
+    public interface IIngedrientService
+    {
+        Task<ViewIngedrientsResult> GetAllIngedrients();
+
+        Task<ViewIngedrientsResult> GetIngedrientById(Guid id);
+
+        Task<ViewIngedrientsResult> GetUnitOfMeasureById(Guid id);
+    }
+}
