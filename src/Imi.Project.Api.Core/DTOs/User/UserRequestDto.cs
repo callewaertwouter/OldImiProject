0f4844809f21413947bd437dac5356fc41d020bd
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 20:45:42 +0200

    Add request dto for users

---
+++ src/Imi.Project.Api.Core/DTOs/User/UserRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Core.DTOs.User
+{
+    public class UserRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "{0} is required!")]
+        public string Email { get; set; }
+
+        //TODO Add list of recipes so they show on requested user
+    }
+}
