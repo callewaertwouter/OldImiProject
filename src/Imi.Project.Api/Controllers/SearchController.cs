6b1a8a3620d7cf43793e83a3a9d966101a7dfefd
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:31:17 +0200

    Add API Controller for searching recipes or users

---
+++ src/Imi.Project.Api/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using Imi.Project.Api.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IRecipeRepository _recipeRepository;
+        private readonly IUserRepository _userRepository;
+
+        public SearchController(IRecipeRepository recipeRepository, 
+                                IUserRepository userRepository)
+        {
+            _recipeRepository = recipeRepository;
+            _userRepository = userRepository;
+        }
+    }
+}
