51b00d82af50287ea6e9c8b2095c9ce812ab74e3
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 22:14:05 +0200

    Add viewmodel for each recipe

---
+++ src/Imi.Project.Shared/Models/RecipeModels/RecipeItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Imi.Project.Shared.Models.RecipeModels
+{
+    public class RecipeItemViewModel : BaseViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        //TODO Add list of recipes
+    }
+}
