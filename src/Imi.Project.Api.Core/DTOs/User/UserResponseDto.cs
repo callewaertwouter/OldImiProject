49bc2ab4f3cbb06003f55224e659b1542b832300
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:29:40 +0200

    Add DTO for users

---
+++ src/Imi.Project.Api.Core/DTOs/User/UserResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using Imi.Project.Api.Core.DTOs.Recipe;
+
+namespace Imi.Project.Api.Core.DTOs.User
+{
+    public class UserResponseDto : BaseDto
+    {
+        public string Email { get; set; }
+        public RecipeResponseDto Recipe { get; set; }
+    }
+}
