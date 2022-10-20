3b3e600bcd9214b7f9607f5af303dacff341cbe7
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:28:24 +0200

    Add view for user service

---
+++ src/Imi.Project.Api.Core/Services/Models/UserService/ViewUsersResult.cs
@@ -0,0 +1,10 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.Services.Models.UserService
+{
+    public class ViewUsersResult : ResultBase
+    {
+        public IEnumerable<User> Users { get; set; }
+    }
+}
