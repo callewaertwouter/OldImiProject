40e19862c92c1db25cef69be5ac1ab7199c269f7
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:27:37 +0200

    Add view for ingedrient service

---
+++ src/Imi.Project.Api.Core/Services/Models/IngedrientService/ViewIngedrientsResult.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.Services.Models.IngedrientService
+{
+    public class ViewIngedrientsResult : ResultBase
+    {
+        public IEnumerable<Ingedrient> Ingedrients { get; set; }
+
+        // Everything related to UnitOfMeasure exists only to give ingedrients an amount (i.e. liter milk)
+        public IEnumerable<UnitOfMeasure> UnitsOfMeasure { get; set; }
+    }
+}
