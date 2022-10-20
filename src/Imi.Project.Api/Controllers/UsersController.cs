0037246b7b1729a22279d669cd44ed2c0c34058f
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:30:58 +0200

    Add API Controller for users

---
+++ src/Imi.Project.Api/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Imi.Project.Api.Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        protected readonly IUserRepository userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+    }
+}
