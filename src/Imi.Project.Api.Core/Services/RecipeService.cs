b041b366121023f2b8f7f8eb552ff9d311120488
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:27:00 +0200

    Add service for recipes

---
+++ src/Imi.Project.Api.Core/Services/RecipeService.cs
@@ -0,0 +1,86 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Infrastructure;
+using Imi.Project.Api.Core.Services.Interfaces;
+using Imi.Project.Api.Core.Services.Models.RecipeService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly IRecipeRepository _recipeRepository;
+
+        public RecipeService(IRecipeRepository recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+
+        public async Task<ViewRecipesResult> GetAllRecipes()
+        {
+            var result = new ViewRecipesResult();
+            result.ValidationResults = new List<ValidationResult>();
+
+            result.Recipes = await _recipeRepository.ListAllAsync();
+            result.IsSuccess = true;
+
+            return result;
+        }
+
+        public async Task<ViewRecipesResult> GetRecipeById(Guid id)
+        {
+            var result = new ViewRecipesResult();
+            var validationResults = new List<ValidationResult>();
+
+            var recipe = await _recipeRepository.GetByIdAsync(id);
+
+            if (recipe == null)
+            {
+                validationResults.Add(new ValidationResult($"Recipe with id {id} is not found."));
+                result.IsSuccess = false;
+            }
+            else
+            {
+                var recipes = new List<Recipe>();
+                recipes.Add(recipe);
+
+                result.Recipes = recipes;
+                result.IsSuccess = true;
+            }
+
+            result.ValidationResults = validationResults;
+
+            return result;
+        }
+
+        public async Task<ViewRecipesResult> SearchRecipeByName(string searchInput)
+        {
+            var result = new ViewRecipesResult();
+            var validationResults = new List<ValidationResult>();
+
+            if (string.IsNullOrWhiteSpace(searchInput))
+            {
+                validationResults.Add(new ValidationResult($"No searchtext provided!"));
+                result.IsSuccess = false;
+            }
+            else if (searchInput.Length <= 1)
+            {
+                validationResults.Add(new ValidationResult($"At least 2 characters are required to search for recipes."));
+                result.IsSuccess = false;
+            }
+            else
+            {
+                var recipes = await _recipeRepository.SearchAsync(searchInput);
+                result.Recipes = recipes;
+                result.IsSuccess = true;
+            }
+
+            result.ValidationResults = validationResults;
+            return result;
+        }
+    }
+}
