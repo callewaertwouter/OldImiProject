8cf38aeb5add1887f2ef2085388c94e6c0110257
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 19:49:56 +0200

    Add get methods to recipe controller

--- src/Imi.Project.Api/Controllers/RecipesController.cs
+++ src/Imi.Project.Api/Controllers/RecipesController.cs
@@ -1,4 +1,6 @@
-﻿using Imi.Project.Api.Core.Infrastructure;
+﻿using Imi.Project.Api.Core.DTOs.Ingedrient;
+using Imi.Project.Api.Core.DTOs.Recipe;
+using Imi.Project.Api.Core.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Imi.Project.Api.Controllers
@@ -8,10 +10,46 @@ namespace Imi.Project.Api.Controllers
     public class RecipesController : ControllerBase
     {
         protected readonly IRecipeRepository _recipeRepository;
+        protected readonly IIngedrientRepository _ingedrientRepository;
 
-        public RecipesController(IRecipeRepository recipeRepository)
+        public RecipesController(IRecipeRepository recipeRepository, 
+                                 IIngedrientRepository ingedrientRepository)
         {
             _recipeRepository = recipeRepository;
+            _ingedrientRepository = ingedrientRepository;
+        }
+
+        //TODO Recipes and ingedrients are many-to-many relationship. Need to figure out how to get all ingedrients for each recipe
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var recipes = await _recipeRepository.ListAllAsync();
+            var recipesDto = recipes.Select(r => new RecipeResponseDto
+            {
+                Id = r.Id,
+                Title = r.Title
+            });
+
+            return Ok(recipesDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var recipe = await _recipeRepository.GetByIdAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound($"No recipe with an id of {id}.");
+            }
+
+            var recipeDto = new RecipeResponseDto
+            {
+                Id = recipe.Id,
+                Title = recipe.Title
+            };
+
+            return Ok(recipeDto);
         }
     }
 }
