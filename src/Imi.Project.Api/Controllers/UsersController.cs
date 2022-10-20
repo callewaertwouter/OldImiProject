60b8983165ea2412cffd61cff3059f5f17bbab59
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 20:06:40 +0200

    Add get methods to user controller

--- src/Imi.Project.Api/Controllers/UsersController.cs
+++ src/Imi.Project.Api/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Imi.Project.Api.Core.Infrastructure;
+﻿using Imi.Project.Api.Core.DTOs.Recipe;
+using Imi.Project.Api.Core.DTOs.User;
+using Imi.Project.Api.Core.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Imi.Project.Api.Controllers
@@ -7,11 +9,48 @@ namespace Imi.Project.Api.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        protected readonly IUserRepository userRepository;
+        protected readonly IUserRepository _userRepository;
+        protected readonly IRecipeRepository _recipeRepository;
 
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository,
+                               IRecipeRepository recipeRepository)
         {
-            this.userRepository = userRepository;
+            _userRepository = userRepository;
+            _recipeRepository = recipeRepository;
+        }
+
+        //TODO Figure how to show get all recipes created by each user (one-to-many relationship)
+        // A recipe can be created by only one user, but one user can create many recipes
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _userRepository.ListAllAsync();
+            var usersDto = users.Select(r => new UserResponseDto
+            {
+                Id = r.Id,
+                Email = r.Email
+            });
+
+            return Ok(usersDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound($"No user with an id of {id}.");
+            }
+
+            var userDto = new UserResponseDto
+            {
+                Id = user.Id,
+                Email = user.Email
+            };
+
+            return Ok(userDto);
         }
     }
 }
