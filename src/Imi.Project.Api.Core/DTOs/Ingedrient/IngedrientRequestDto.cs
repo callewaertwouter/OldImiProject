dd906783a4bec8787c528fd5fef1d9f328bb4928
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 20:40:37 +0200

    Add request dto for ingedrients

---
+++ src/Imi.Project.Api.Core/DTOs/Ingedrient/IngedrientRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Core.DTOs.Ingedrient
+{
+    public class IngedrientRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "{0} is required!")]
+        public string Name { get; set; }
+    }
+}
