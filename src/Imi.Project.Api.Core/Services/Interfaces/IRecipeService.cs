d191a7a0a2a91b40c5278a379d47370a318c8166
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:26:45 +0200

    Add interface for recipe service

---
+++ src/Imi.Project.Api.Core/Services/Interfaces/IRecipeService.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Services.Models.RecipeService;
+using System;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Services.Interfaces
+{
+    public interface IRecipeService
+    {
+        Task<ViewRecipesResult> GetAllRecipes();
+        Task<ViewRecipesResult> GetRecipeById(Guid id);
+        Task<ViewRecipesResult> SearchRecipeByName(string searchInput);
+    }
+}
