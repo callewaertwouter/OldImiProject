b02f2385dd249e1453ddc7ba04e81b5ac671fc30
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:28:35 +0200

    Add interface for user service

---
+++ src/Imi.Project.Api.Core/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Services.Models.UserService;
+using System;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<ViewUsersResult> GetAllUsers();
+        Task<ViewUsersResult> GetUserById(Guid id);
+        Task<ViewUsersResult> SearchUserByName(string searchInput);
+    }
+}
