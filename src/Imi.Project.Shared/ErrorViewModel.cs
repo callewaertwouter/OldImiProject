29cecdc8f247a5553e22208e69047efeed8fdb6b
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 22:08:19 +0200

    Add viewmodels for errors

---
+++ src/Imi.Project.Shared/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Imi.Project.Shared
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
