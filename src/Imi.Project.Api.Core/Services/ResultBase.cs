29f0c26e9e6e7e883f5a0319bad030a1e4c6c35d
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:27:17 +0200

    Add base for services

---
+++ src/Imi.Project.Api.Core/Services/ResultBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Core.Services
+{
+    public abstract class ResultBase
+    {
+        protected IEnumerable<ValidationResult> _validationResults;
+
+        /// <summary>
+        /// Holds all validation errors in this result
+        /// </summary>
+        public IEnumerable<ValidationResult> ValidationResults { get; set; } = new ValidationResult[0];
+
+        /// <summary>
+        /// Gets or sets a value whether this result represents a succesful action
+        /// </summary>
+        public bool IsSuccess { get; set; }
+    }
+}
