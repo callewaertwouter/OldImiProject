3ea0259c1c03b83bfbfd4174ea09b317a14281f1
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 17:26:11 +0200

    Add service view for recipes

---
+++ src/Imi.Project.Api.Core/Services/Models/RecipeService/ViewRecipesResult.cs
@@ -0,0 +1,10 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System.Collections.Generic;
+
+namespace Imi.Project.Api.Core.Services.Models.RecipeService
+{
+    public class ViewRecipesResult : ResultBase
+    {
+        public IEnumerable<Recipe> Recipes { get; set; }
+    }
+}
