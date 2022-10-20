63815b6da2a429e5e60b3972fce95be8fce2724f
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 21:12:23 +0200

    Add Swagger

--- src/Imi.Project.Api/Program.cs
+++ src/Imi.Project.Api/Program.cs
@@ -24,13 +24,16 @@ builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseCors(builder => builder.AllowAnyOrigin()
         .AllowAnyHeader()
