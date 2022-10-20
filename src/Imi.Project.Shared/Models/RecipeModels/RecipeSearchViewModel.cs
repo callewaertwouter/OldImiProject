ae4ba07b7bc9b229cbda1a867a663d923e4fac14
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 22:17:50 +0200

    Add viewmodel for searched recipe

---
+++ src/Imi.Project.Shared/Models/RecipeModels/RecipeSearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Imi.Project.Shared.Models.RecipeModels
+{
+    public class RecipeSearchViewModel : BaseViewModel
+    {
+        public string SearchText { get; set; }
+
+        public List<RecipeItemViewModel> Recipes { get; set; }
+    }
+}
