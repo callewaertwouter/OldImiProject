b3acaed1ea332608d414a8b3a4d79a3545bd1473
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 22:00:08 +0200

    Add method to show errormessages

--- src/Imi.Project.Wpf/MainWindow.xaml.cs
+++ src/Imi.Project.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Imi.Project.Api.Core.Services.Interfaces;
+using Imi.Project.Api.Core.Services.Models.RecipeService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,14 +31,27 @@ namespace Imi.Project.Wpf
             InitializeComponent();
         }
 
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void ShowErrorMessages(ViewRecipesResult result)
         {
+            StringBuilder errorMessages = new StringBuilder();
 
+            foreach (var error in result.ValidationResults)
+            {
+                errorMessages.Append(error.ErrorMessage);
+            }
+
+            lblFeedback.Content = errorMessages;
+            lblFeedback.Background = Brushes.OrangeRed;
         }
     }
 }
