fe4211848d071cacb52d330f1043e392162943b1
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-20 20:43:10 +0200

    Add request dto for recipes

---
+++ src/Imi.Project.Api.Core/DTOs/Recipe/RecipeRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Core.DTOs.Recipe
+{
+    public class RecipeRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "{0} is required!")]
+        public string Title { get; set; }
+
+        //TODO Add list of ingedrients so they show on requested recipe
+    }
+}
