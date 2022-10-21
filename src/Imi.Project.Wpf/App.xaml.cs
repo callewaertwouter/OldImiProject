3ca5345ff578b23a70d417f391b6c4db86d28fcb
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-21 15:48:50 +0200

    Implement configuration to start up Wpf app

--- src/Imi.Project.Wpf/App.xaml.cs
+++ src/Imi.Project.Wpf/App.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Imi.Project.Api.Core.Infrastructure;
+using Imi.Project.Api.Core.Services;
+using Imi.Project.Api.Core.Services.Interfaces;
+using Imi.Project.Api.Infrastructure.Data;
+using Imi.Project.Api.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 using System.Windows;
 
 namespace Imi.Project.Wpf
@@ -13,5 +17,34 @@ namespace Imi.Project.Wpf
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ImiDb")));
+
+            services.AddScoped<IRecipeRepository, RecipeRepository>();
+            services.AddScoped<IRecipeService, RecipeService>();
+            services.AddTransient(typeof(MainWindow));
+        }
     }
 }
