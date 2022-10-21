a245184b612f1aa14a7ae8d53da2c9058668bdee
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-21 21:21:47 +0200

    Add extra properties

--- src/Imi.Project.Api.Core/DTOs/Recipe/RecipeResponseDto.cs
+++ src/Imi.Project.Api.Core/DTOs/Recipe/RecipeResponseDto.cs
@@ -1,10 +1,13 @@
 ﻿using Imi.Project.Api.Core.DTOs.Ingedrient;
+using Imi.Project.Api.Core.DTOs.User;
 
 namespace Imi.Project.Api.Core.DTOs.Recipe
 {
     public class RecipeResponseDto : BaseDto
     {
         public string Title { get; set; }
+        public string Description { get; set; }
         public IngedrientResponseDto Ingedrients { get; set; }
+        public UserResponseDto CreatedByUser { get; set; }
     }
 }
