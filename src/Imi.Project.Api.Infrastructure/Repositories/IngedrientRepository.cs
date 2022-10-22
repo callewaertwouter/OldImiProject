15a8c831c89dad279a4c707652b73f51c50e7b15
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-22 16:01:16 +0200

    Override base methods

--- src/Imi.Project.Api.Infrastructure/Repositories/IngedrientRepository.cs
+++ src/Imi.Project.Api.Infrastructure/Repositories/IngedrientRepository.cs
@@ -1,6 +1,12 @@
 ﻿using Imi.Project.Api.Core.Entities;
 using Imi.Project.Api.Core.Infrastructure;
 using Imi.Project.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Infrastructure.Repositories
 {
@@ -8,7 +14,26 @@ namespace Imi.Project.Api.Infrastructure.Repositories
     {
         public IngedrientRepository(AppDbContext dbContext) : base(dbContext)
         {
+            
+        }
 
+        public override IQueryable<Ingedrient> GetAll()
+        {
+            return _dbContext.Ingedrients.Include(i => i.UnitOfMeasure);
+        }
+
+        public async override Task<IEnumerable<Ingedrient>> ListAllAsync()
+        {
+            var ingedrients = await GetAll().ToListAsync();
+
+            return ingedrients;
+        }
+
+        public async override Task<Ingedrient> GetByIdAsync(Guid id)
+        {
+            var ingedrient = await GetAll().SingleOrDefaultAsync(r => r.Id.Equals(id));
+
+            return ingedrient;
         }
     }
 }
