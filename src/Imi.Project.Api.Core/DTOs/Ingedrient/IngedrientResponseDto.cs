21b95772be92397f6c6f30ccf3a8ef06fe5055ed
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-22 16:00:51 +0200

    Add measure unit property

--- src/Imi.Project.Api.Core/DTOs/Ingedrient/IngedrientResponseDto.cs
+++ src/Imi.Project.Api.Core/DTOs/Ingedrient/IngedrientResponseDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Imi.Project.Api.Core.DTOs.Ingedrient
+﻿using Imi.Project.Api.Core.DTOs.UnitOfMeasure;
+
+namespace Imi.Project.Api.Core.DTOs.Ingedrient
 {
     public class IngedrientResponseDto : BaseDto
     {
         public string Name { get; set; }
+        public UnitOfMeasureResponseDto MeasureUnit { get; set; }
     }
 }
