c1ff76256310701a42983627845b1fd957e1b68b
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-25 22:09:01 +0200

    Create folder for crud services

---
+++ src/Imi.Project.Blazor/Services/Crud/BlazorRecipeService.cs
@@ -0,0 +1,61 @@
+﻿using Imi.Project.Api.Core.Entities;
+
+namespace Imi.Project.Blazor.Services.Crud
+{
+    public class BlazorRecipeService : ICRUDService<Recipe>
+    {
+        static List<Recipe> recipes = new List<Recipe>();
+
+        public Task<Recipe> Get(Guid id)
+        {
+            return Task.FromResult(
+                recipes.SingleOrDefault(x => x.Id == id)
+                );
+        }
+
+        public Task<IQueryable<Recipe>> GetAll()
+        {
+            return Task.FromResult(
+                recipes.Select(x => new Recipe()
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Description = x.Description,
+                    Ingedrients = x.Ingedrients,
+                    User = x.User
+                }).AsQueryable()
+                );
+        }
+
+        public Task Create(Recipe item)
+        {
+            item.Id = Guid.NewGuid();
+            recipes.Add(item);
+            return Task.CompletedTask;
+        }
+
+        public Task Update(Recipe item)
+        {
+            var recipe = recipes.SingleOrDefault(x => x.Id == item.Id);
+
+            if (recipe == null) throw new ArgumentException("Recipe not found...");
+
+            recipe.Title = item.Title;
+            recipe.Description = item.Description;
+            recipe.Ingedrients = item.Ingedrients;
+            recipe.User = item.User;
+
+            return Task.CompletedTask;
+        }
+
+        public Task Delete(Guid id)
+        {
+            var recipe = recipes.SingleOrDefault(x => x.Id == id);
+
+            if (recipe == null) throw new ArgumentException("Recipe not found...");
+
+            recipes.Remove(recipe);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Imi.Project.Blazor/Services/Crud/ICRUDService.cs
@@ -0,0 +1,15 @@
+﻿namespace Imi.Project.Blazor.Services.Crud
+{
+    public interface ICRUDService<T> where T : class
+    {
+        Task<IQueryable<T>> GetAll();
+
+        Task<T> Get(Guid id);
+
+        Task Create(T item);
+
+        Task Update(T item);
+
+        Task Delete(Guid id);
+    }
+}
