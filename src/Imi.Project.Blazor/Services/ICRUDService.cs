e59dd223ea27bdb81738a1a7518eab867a0b4dec
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-25 20:30:18 +0200

    Add crud service interface

---
+++ src/Imi.Project.Blazor/Services/ICRUDService.cs
@@ -0,0 +1,15 @@
+﻿namespace Imi.Project.Blazor.Services
+{
+    public interface ICRUDService<T> where T : class
+    {
+        Task<IQueryable<T>> GetAll();
+
+        Task<T> Get(Guid id);
+
+        Task Create(T item);
+
+        Task Update(T item);
+
+        Task Delete(Guid id);
+    }
+}
