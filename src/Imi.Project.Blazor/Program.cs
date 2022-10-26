61d9dadda90650ca86b8ce76c54ee89b7fcae616
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-26 15:37:03 +0200

    Update scopes with mock entities

--- src/Imi.Project.Blazor/Program.cs
+++ src/Imi.Project.Blazor/Program.cs
@@ -1,4 +1,4 @@
-using Imi.Project.Api.Core.Entities;
+using Imi.Project.Blazor.Models;
 using Imi.Project.Blazor.Services.Concepts;
 using Imi.Project.Blazor.Services.Crud;
 
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddTransient<ICRUDService<Recipe>, BlazorRecipeService>();
-builder.Services.AddTransient<ICRUDService<User>, BlazorUserService>();
+builder.Services.AddTransient<ICRUDService<MockRecipe>, BlazorRecipeService>();
+builder.Services.AddTransient<ICRUDService<MockUser>, BlazorUserService>();
 builder.Services.AddTransient<IQuizService, QuizService>();
 
 var app = builder.Build();
