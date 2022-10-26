8c6de386509f7acbf1f76fac20928c7cb7e75129
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-26 15:35:44 +0200

    Create mock entities

---
+++ src/Imi.Project.Blazor/Models/MockRecipe.cs
@@ -0,0 +1,8 @@
+﻿namespace Imi.Project.Blazor.Models
+{
+    public class MockRecipe : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Imi.Project.Blazor/Models/MockUser.cs
@@ -0,0 +1,7 @@
+﻿namespace Imi.Project.Blazor.Models
+{
+    public class MockUser : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
