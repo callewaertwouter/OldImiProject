214bbb4fb22d3265084e8d074775ed9e4059bde6
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-27 12:35:57 +0200

    Add navigation to registerpage

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRegister_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MenuPage(), true);
+        }
+
+        private async void btnBack_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage(), true);
+        }
+    }
+}
