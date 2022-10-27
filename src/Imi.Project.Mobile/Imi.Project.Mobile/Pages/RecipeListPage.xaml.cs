1753e41792882bf99165989332b75ba825003a1e
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-27 15:27:40 +0200

    Add navigation to recipe list page

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RecipeListPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecipeListPage : ContentPage
+    {
+        public RecipeListPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnBack_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MenuPage(), true);
+        }
+    }
+}
