0fcbcd676a21d479871f0d7c0b318fa6ee0f3228
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-29 16:21:13 +0200

    Add service interfaces

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/IRecipeService.cs
@@ -0,0 +1,21 @@
+﻿using Imi.Project.Mobile.Domain.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Mobile.Domain.Services
+{
+    public interface IRecipeService
+    {
+        Task<Recipe> GetRecipe(Guid id);
+
+        Task<Recipe> GetAllRecipes();
+
+        Task<Recipe> AddRecipe();
+
+        Task<Recipe> UpdateRecipe(Recipe recipe);
+
+        Task<Recipe> DeleteRecipe(Guid id);
+
+        //TODO: Maybe another method to get all recipes from one user
+    }
+}
---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Mobile.Domain.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Mobile.Domain.Services
+{
+    public interface IUserService
+    {
+        Task<User> GetUser(Guid id);
+
+        //TODO: Maybe add UpdateUser and DeleteUser later if required or enough time left in december
+    }
+}
