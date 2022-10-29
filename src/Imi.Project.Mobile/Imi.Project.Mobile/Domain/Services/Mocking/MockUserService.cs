c471b5ba53edde407b7c58124e568a2eee41a2b9
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-29 16:32:43 +0200

    Add mocking service for users

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Mocking/MockUserService.cs
@@ -0,0 +1,23 @@
+﻿using Imi.Project.Mobile.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Mobile.Domain.Services.Mocking
+{
+    public class MockUserService : IUserService
+    {
+        private static List<User> users = new List<User>
+        {
+            new User{ Id=Guid.Parse("00000000-0000-0000-0000-000000000001"), Email="woutercallewaert@gmail.com" },
+            new User{ Id=Guid.Parse("00000000-0000-0000-0000-000000000002"), Email="admin@admin.be" }
+        };
+
+        public async Task<User> GetUser(Guid id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            return await Task.FromResult(user);
+        }
+    }
+}
