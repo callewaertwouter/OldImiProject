da212e06f59c72954e9bcf82c62cbecd585c1242
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-29 16:16:10 +0200

    Add domain models

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Imi.Project.Mobile.Domain.Models
+{
+    public class Recipe
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public User CreatedByUser { get; set; }
+
+        //TODO: Add list of ingedrients which is borrowed from the database (API connection required)
+    }
+}
---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Imi.Project.Mobile.Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+    }
+}
