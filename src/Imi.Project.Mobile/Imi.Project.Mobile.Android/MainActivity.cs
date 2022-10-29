ec3f1f02ae2371288601f78bb7ab1082cbdb0701
woutercallewaert <wouter.callewaert@student.howest.be>
2022-10-29 16:04:56 +0200

    Remove icons preventing startup

--- src/Imi.Project.Mobile/Imi.Project.Mobile.Android/MainActivity.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Imi.Project.Mobile.Droid
 {
-    [Activity(Label = "Imi.Project.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Persoonlijke recepten", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
