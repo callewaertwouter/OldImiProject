9208c4981c3ad823eb447f855847d2e8f5d41155
woutercallewaert <wouter.callewaert@student.howest.be>
2022-11-13 15:28:24 +0100

    Add user register dto

---
+++ src/Imi.Project.Api.Core/DTOs/User/RegisterUserRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Api.Core.DTOs.User
+{
+    public class RegisterUserRequestDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Both password fields do not match each other!")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+    }
+}
