7326fb506324bc0fa7f30b180df18f702cfdcccd
woutercallewaert <wouter.callewaert@student.howest.be>
2022-11-20 17:03:56 +0100

    Create minimum age class

---
+++ src/Imi.Project.Api/Policies/MinimumAgeRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Imi.Project.Api.Policies
+{
+    public class MinimumAgeRequirement : IAuthorizationRequirement
+    {
+        public MinimumAgeRequirement(int age)
+        {
+            MinimumAge = age;
+        }
+
+        public int MinimumAge { get; set; }
+    }
+}
