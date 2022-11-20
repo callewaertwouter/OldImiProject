81300b00e1130bbfd0aae1491aa5b5907623f41a
woutercallewaert <wouter.callewaert@student.howest.be>
2022-11-20 15:47:04 +0100

    Add properties

--- src/Imi.Project.Api.Core/DTOs/Recipe/RecipeRequestDto.cs
+++ src/Imi.Project.Api.Core/DTOs/Recipe/RecipeRequestDto.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Imi.Project.Api.Core.DTOs.Ingedrient;
+using Imi.Project.Api.Core.DTOs.User;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Imi.Project.Api.Core.DTOs.Recipe
 {
@@ -6,5 +9,8 @@ namespace Imi.Project.Api.Core.DTOs.Recipe
     {
         [Required(ErrorMessage = "{0} is required!")]
         public string Title { get; set; }
+        public string Description { get; set; }
+        public ICollection<IngedrientResponseDto> Ingedrients { get; set; }
+        public UserResponseDto CreatedBy { get; set; }
     }
 }
