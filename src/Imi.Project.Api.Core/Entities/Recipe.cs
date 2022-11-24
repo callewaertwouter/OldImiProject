debec59db1ee0908f4976c5be4e75993a471a2da
woutercallewaert <woutercallewaert@gmail.com>
2022-11-24 14:24:13 +0100

    Change property

--- src/Imi.Project.Api.Core/Entities/Recipe.cs
+++ src/Imi.Project.Api.Core/Entities/Recipe.cs
@@ -15,7 +15,7 @@ namespace Imi.Project.Api.Core.Entities
         // Many recipes will use many ingedriënts
         public ICollection<Ingedrient> Ingedrients { get; set; }
 
-        public Guid UserId { get; set; }
+        public string UserId { get; set; }
         public User User { get; set; }
     }
 }
