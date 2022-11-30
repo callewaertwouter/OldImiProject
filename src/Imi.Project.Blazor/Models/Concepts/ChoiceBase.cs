88a1c50a41951fe2f43765685365d4adb0681f98
woutercallewaert <wouter.callewaert@student.howest.be>
2022-11-30 18:35:42 +0100

    Add class for quiz choices

---
+++ src/Imi.Project.Blazor/Models/Concepts/ChoiceBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Imi.Project.Blazor.Models.Concepts
+{
+	// Naming this ChoiceBase instead of Choice.cs else it might clash names with Choice.razor
+	public class ChoiceBase : ComponentBase
+	{
+		[Parameter]
+		public string Choice { get; set; } = string.Empty;
+
+		[Parameter]
+		public EventCallback<string> OnSelectedChoice { get; set; }
+
+		protected async void SelectChoice()
+		{
+			await OnSelectedChoice.InvokeAsync(Choice);
+		}
+	}
+}
