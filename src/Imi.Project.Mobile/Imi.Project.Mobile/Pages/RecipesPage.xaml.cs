d250f94f9c1d4aadcfb1be10bff66737f47aa6df
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:48:25 +0100

    Remove button code

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RecipesPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Imi.Project.Mobile.Domain.Models;
+using Imi.Project.Mobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecipesPage : ContentPage
+    {
+        public RecipesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var viewModel = (RecipesViewModel) BindingContext;
+            var selectedRecipe = (Recipe)e.Item;
+
+            viewModel.OpenRecipeDetailCommand.Execute(selectedRecipe.Id);
+        }
+    }
+}
