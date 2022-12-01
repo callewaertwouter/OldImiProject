9a8c7d698f7ef005f13a22ff9e1c61b9007d8a46
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:49:42 +0100

    Create viewmodel for creating recipes page

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/RecipeCreationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class RecipeCreationViewModel : FreshBasePageModel
+    {
+        public RecipeCreationViewModel()
+        {
+
+        }
+
+        public ICommand OpenRecipeDetailCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<RecipeDetailViewModel>(true);
+            }
+            );
+
+        public ICommand OpenMenuPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<MenuViewModel>(true);
+            }
+            );
+    }
+}
