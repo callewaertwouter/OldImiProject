c38b23848f02357eee915f40ccc115d413fbe9f0
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:50:25 +0100

    Create viewmodel for register page

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class RegisterViewModel : FreshBasePageModel
+    {
+        public RegisterViewModel()
+        {
+
+        }
+
+        public ICommand OpenMenuPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<MenuViewModel>(true);
+            }
+            );        
+        
+        public ICommand OpenMainPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<MainViewModel>(true);
+            }
+            );
+    }
+}
