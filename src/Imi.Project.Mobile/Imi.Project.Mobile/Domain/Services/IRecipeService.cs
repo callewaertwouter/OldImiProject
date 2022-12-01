7d2bb7e81b7cb208be74210d1211fb6100969d99
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:52:04 +0100

    Change service functions

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/IRecipeService.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/IRecipeService.cs
@@ -1,14 +1,15 @@
 ﻿using Imi.Project.Mobile.Domain.Models;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Mobile.Domain.Services
 {
     public interface IRecipeService
     {
-        Task<Recipe> GetRecipe(Guid id);
+        Recipe GetRecipe(Guid id);
 
-        Task<Recipe> GetAllRecipes();
+        IEnumerable<Recipe> GetRecipes();
 
         Task<Recipe> AddRecipe(Recipe recipe);
 
