ddb2e9574e2697b4a6042d7be56fa0b78cbce723
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 22:57:11 +0100

    Add commented task list

--- src/Imi.Project.Mobile/Imi.Project.Mobile/App.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/App.xaml.cs
@@ -30,3 +30,12 @@ namespace Imi.Project.Mobile
         }
     }
 }
+
+/* TODO: Vragen voor MDE
+ *  1) Voor Speech-To-Text heb ik de implementatie volgens het boekje gevolgd, maar ik krijg een "object reference not set to instance of an object"
+ *     Daarnaast wordt de knop niet uitgeschakeld op UWP terwijl het nochtans zo gecodeerd staat
+ *     
+ *  2) Voor online strategie koos ik "Online CRUD operaties met eigen REST API". Nu ben ik niet zeker of dit kan en mag verwezen worden naar de API
+ *     van PRI. Zo ja, neem ik aan dat ik dan al de connectie met de API van PRI zal moeten maken?
+ *              Zo nee, ben ik niet zeker welke API ik dan wel moet gebruiken. Die van TheMealDB zoals ik in mijn voorstel genoteerd had? Of nog iets anders?
+ */
