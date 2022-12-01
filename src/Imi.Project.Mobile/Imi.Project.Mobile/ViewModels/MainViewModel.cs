14fef2e21b99c816ffb7d853a9acafd2998ebeda
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:45:27 +0100

    Create viewmodel for mainpage

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using FreshMvvm;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class MainViewModel : FreshBasePageModel
+    {
+        public MainViewModel()
+        {
+
+        }
+
+        public ICommand OpenRegisterPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<RegisterViewModel>(true);
+            }
+            );
+
+        public ICommand OpenMenuPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<MenuViewModel>(true);
+            }
+            );
+    }
+}
