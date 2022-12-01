4774c0531eb754118c9aec92f5a20de436cd3d58
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:49:21 +0100

    Remove button code

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RecipeDetailPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecipeDetailPage : ContentPage
+    {
+        public RecipeDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
