63c09dcd2fa17cc364eb997266fbd859e2dffc39
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:50:13 +0100

    Remove button code

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RecipeCreationPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RecipeCreationPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Imi.Project.Mobile.Pages
@@ -10,16 +9,6 @@ namespace Imi.Project.Mobile.Pages
         public RecipeCreationPage()
         {
             InitializeComponent();
-        }
-
-        private async void btnAdd_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new RecipePage(), true);
-        }
-
-        private async void btnBack_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new MenuPage(), true);
         }
     }
 }
