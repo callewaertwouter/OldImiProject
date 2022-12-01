816fa2a3596503c46181802a066ecbfd50519962
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 21:36:26 +0100

    Apply Speech-To-Text implementation

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RecipeCreationPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RecipeCreationPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using Imi.Project.Mobile.Domain.Services;
+using Imi.Project.Mobile.Domain.Services.Features;
+using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Imi.Project.Mobile.Pages
@@ -6,9 +9,58 @@ namespace Imi.Project.Mobile.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RecipeCreationPage : ContentPage
     {
+        private ISpeechToText _speechToText;
+
         public RecipeCreationPage()
         {
             InitializeComponent();
+
+            try
+            {
+                _speechToText = DependencyService.Get<ISpeechToText>();
+            }
+            catch (Exception ex)
+            {
+                lblSpeechToText.Text = ex.Message;
+            }
+
+            MessagingCenter.Subscribe<ISpeechToText, string>(this, "STT", (sender, args) =>
+            {
+                btnSpeechToTextFinalResultReceived(args);
+            });
+
+            MessagingCenter.Subscribe<ISpeechToText>(this, "Final", (sender) =>
+            {
+                btnSpeechToText.IsEnabled = true;
+            });
+
+            MessagingCenter.Subscribe<IMessageSender, string>(this, "STT", (sender, args) =>
+            {
+                btnSpeechToTextFinalResultReceived(args);
+            });
+        }
+
+        private void btnSpeechToTextFinalResultReceived(string args)
+        {
+            lblSpeechToText.Text = args;
+        }
+
+        private void btnSpeechToText_Clicked(object sender, EventArgs e)
+        {
+            // Speech-to-Text werkt enkel als het op Android is. Indien IOS of UWP, zal de button niet werken.
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.UWP)
+            {
+                btnSpeechToText.IsEnabled = false;
+            }
+
+            try
+            {
+                _speechToText.StartSpeechToText();
+            }
+            catch (Exception ex)
+            {
+                lblSpeechToText.Text = ex.Message;
+            }
         }
     }
 }
