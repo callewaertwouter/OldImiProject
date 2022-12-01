0128575b970d8c2968297d8a2d93c63208d145ca
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:46:37 +0100

    Create viewmodel for menupage

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/MenuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Imi.Project.Mobile.ViewModels
+{
+    public class MenuViewModel : FreshBasePageModel
+    {
+        public MenuViewModel()
+        {
+
+        }
+
+        public ICommand OpenRecipeCreationPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<RecipeCreationViewModel>(true);
+            }
+            );
+
+        public ICommand OpenRecipesPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<RecipesViewModel>(true);
+            }
+            );
+
+        public ICommand OpenMenuPageCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<MenuViewModel>(true);
+            }
+            );
+    }
+}
