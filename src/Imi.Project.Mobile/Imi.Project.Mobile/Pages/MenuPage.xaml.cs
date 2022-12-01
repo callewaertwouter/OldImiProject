2b9dce84a97693cf187a1f0e0bc1f3a99eb41ac3
woutercallewaert <wouter.callewaert@student.howest.be>
2022-12-01 17:46:58 +0100

    Remove button code

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/MenuPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/MenuPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Imi.Project.Mobile.Pages
@@ -10,31 +9,6 @@ namespace Imi.Project.Mobile.Pages
         public MenuPage()
         {
             InitializeComponent();
-        }
-
-        private async void btnAddRecipe_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new RecipeCreationPage(), true);
-        }
-
-        private async void btnViewRecipes_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new RecipeListPage(), true);
-        }
-
-        private async void btnQuiz_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new QuizPage(), true);
-        }
-
-        private async void btnQuit_Clicked(object sender, EventArgs e)
-        {
-            bool goQuit = await DisplayAlert("Terugkeren?", "Weet je zeker dat je de app wilt afsluiten?", "Ja", "Nee");
-
-            if (goQuit)
-            {
-                Environment.Exit(0);
-            }
         }
     }
 }
