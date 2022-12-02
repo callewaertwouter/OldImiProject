9e831c02f69e128004ba62851279ef4fcab6905c
woutercallewaert <woutercallewaert@gmail.com>
2022-12-02 10:44:27 +0100

    Add command and OpenBrowser function

--- src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/QuizViewModel.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/ViewModels/QuizViewModel.cs
@@ -1,4 +1,9 @@
 ﻿using FreshMvvm;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace Imi.Project.Mobile.ViewModels
 {
@@ -7,6 +12,36 @@ namespace Imi.Project.Mobile.ViewModels
         public QuizViewModel()
         {
 
+        }
+
+        public ICommand OpenBlazorQuizCommand => new Command(
+            async () =>
+            {
+                await OpenBrowser(new Uri("https://localhost:7219/quiz"));
+            });
+
+        // Xamarin Essentials: Open Browser
+        // https://learn.microsoft.com/en-us/xamarin/essentials/open-browser?tabs=uwp
+        public async Task OpenBrowser(Uri uri)
+        {
+            var current = Connectivity.NetworkAccess;
+
+            try
+            {
+                if (current == NetworkAccess.Internet)
+                {
+                    await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+                }
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Probleem!", "Start ook Blazor op om de quiz te bekijken.", "Ok");
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
