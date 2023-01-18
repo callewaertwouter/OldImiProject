327456600f24c50cd0c44f3f42acf230f797b873
woutercallewaert <wouter.callewaert@student.howest.be>
2023-01-18 12:19:29 +0100

    Update mock service with modified parameters

--- src/Imi.Project.Blazor/Services/Crud/BlazorRecipeService.cs
+++ src/Imi.Project.Blazor/Services/Crud/BlazorRecipeService.cs
@@ -7,12 +7,12 @@ namespace Imi.Project.Blazor.Services.Crud
     {
         static List<MockRecipe> recipes = new List<MockRecipe>
         {
-            new MockRecipe() { Id=1, Title="Gehaktbrood", Description="Meng gehakt en ajuin in een schaal en mix ze. Smijt de mix in een ovenpan en herschaap het tot een brood. Doe er vervolgens paneermeel en boter over. 20 minuten in de combi-oven. Kook de patatten en de bonen. Smakelijk." },
-            new MockRecipe() { Id=2, Title="Witte saus", Description="Maak een roux van de boter en de bloem. Dit doe je door de boter in een steelpannetje te smelten. Doe de bloem bij de boter en roer met de garde. Laat het mengsel een beetje opdrogen in het pannetje, tot je de geur van koekjes ruikt. Giet er beetje bij beetje de koude melk bij en roer telkens het mengsel glad. Breng al roerend aan de kook, op matig vuur. Laat de witte saus indikken en laat nog enkele minuten doorkoken zodat de bloemsmaak verdwijnt. Breng op smaak met nootmuskaat, peper en zout." },
-            new MockRecipe() { Id=2, Title="Hamburger", Description="Wortelen snijden en koken. Kook de patatten. Doe de hamburger in een pan en laat het bakken. Easy-peasy." }
+            new MockRecipe() { Id=Guid.NewGuid(), Title="Gehaktbrood", Description="Meng gehakt en ajuin in een schaal en mix ze. Smijt de mix in een ovenpan en herschaap het tot een brood. Doe er vervolgens paneermeel en boter over. 20 minuten in de combi-oven. Kook de patatten en de bonen. Smakelijk." },
+            new MockRecipe() { Id=Guid.NewGuid(), Title="Witte saus", Description="Maak een roux van de boter en de bloem. Dit doe je door de boter in een steelpannetje te smelten. Doe de bloem bij de boter en roer met de garde. Laat het mengsel een beetje opdrogen in het pannetje, tot je de geur van koekjes ruikt. Giet er beetje bij beetje de koude melk bij en roer telkens het mengsel glad. Breng al roerend aan de kook, op matig vuur. Laat de witte saus indikken en laat nog enkele minuten doorkoken zodat de bloemsmaak verdwijnt. Breng op smaak met nootmuskaat, peper en zout." },
+            new MockRecipe() { Id=Guid.NewGuid(), Title="Hamburger", Description="Wortelen snijden en koken. Kook de patatten. Doe de hamburger in een pan en laat het bakken. Easy-peasy." }
         };
 
-        public Task<MockRecipe> Get(int id)
+        public Task<MockRecipe> Get(Guid id)
         {
             return Task.FromResult(
                 recipes.SingleOrDefault(x => x.Id == id)
@@ -33,7 +33,8 @@ namespace Imi.Project.Blazor.Services.Crud
 
         public Task Create(MockRecipe item)
         {
-            item.Id = recipes.Count() > 0 ? recipes.Max(x => x.Id) + 1 : 1;
+            //item.Id = recipes.Count() > 0 ? recipes.Max(x => x.Id) + 1 : 1;
+            item.Id = Guid.NewGuid();
             recipes.Add(item);
             return Task.CompletedTask;
         }
@@ -50,7 +51,7 @@ namespace Imi.Project.Blazor.Services.Crud
             return Task.CompletedTask;
         }
 
-        public Task Delete(int id)
+        public Task Delete(Guid id)
         {
             var recipe = recipes.SingleOrDefault(x => x.Id == id);
 
