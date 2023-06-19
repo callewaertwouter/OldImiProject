90256385bf8189cde7f5a392c216f05b0f0a1600
woutercallewaert <wouter.callewaert@student.howest.be>
2023-06-19 21:15:49 +0200

    Add more questions and randomizer

--- src/Imi.Project.Blazor/Models/Concepts/QuizBase.cs
+++ src/Imi.Project.Blazor/Models/Concepts/QuizBase.cs
@@ -30,49 +30,77 @@ namespace Imi.Project.Blazor.Models.Concepts
 
         private void LoadQuestions()
         {
-            Question question1 = new Question
+            List<Question> questionsList = new List<Question>()
+            {
+            new Question
             {
                 QuestionTitle = "Tot welke soort behoort keukenzout?",
                 Choices = new List<string>() { "Kruid", "Smaakversterker", "Groente" },
                 Answer = "Smaakversterker"
-            };
+            },
 
-            Question question2 = new Question
+            new Question
             {
                 QuestionTitle = "Waar komen hotdogs origineel van?",
                 Choices = new List<string>() { "Amerika", "Engeland", "Ierland", "Duitsland" },
                 Answer = "Duitsland"
-            };
+            },
 
-            Question question3 = new Question
+            new Question
             {
                 QuestionTitle = "Wat betekent het japanse woord 'Nori'?",
                 Choices = new List<string>() { "Zeewier", "Boter", "Mossel", "Vogelborst" },
                 Answer = "Zeewier"
-            };
+            },
 
-            Question question4 = new Question
+            new Question
             {
                 QuestionTitle = "Welke saus wordt traditioneel gebruikt voor eieren Benedict?",
                 Choices = new List<string>() { "Witte saus", "Hollandaisesaus", "Barbecausaus" },
                 Answer = "Hollandaisesaus"
-            };
+            },
 
-            Question question5 = new Question
+            new Question
             {
                 QuestionTitle = "Waar komt graan vandaan?",
                 Choices = new List<string>() { "Van de bomen", "Van de struiken", "Van de velden", "Van de dieren" },
                 Answer = "Van de velden"
-            };
+            },
 
-            Question question6 = new Question
+            new Question
+            {
+                QuestionTitle = "Van welk continent komt Chicken Tikka Masala?",
+                Choices = new List<string>() { "Europa", "Azië", "Afrika" },
+                Answer = "Azië"
+            },
+
+            new Question
+            {
+                QuestionTitle = "Wat is het hoofdingedriënt van bier?",
+                Choices = new List<string>() { "Hops", "Graan", "Water", "Alcohol" },
+                Answer = "Water"
+            },
+
+            new Question
+            {
+                QuestionTitle = "Dat Japanners van vis houden, is algemene kennis. Maar naar welke vissoort verwijst 'Fugu'?",
+                Choices = new List<string>() { "Tonijn", "Inktvis", "Kogelvis", "Dolfijn" },
+                Answer = "Kogelvis"
+            },
+
+            new Question
             {
                 QuestionTitle = "Is deze quiz de beste quiz ooit?",
                 Choices = new List<string>() { "Jaaa!!!", "Nee zeg..." },
                 Answer = "Jaaa!!!"
-            };
+            }
+        };
 
-            Questions.AddRange(new List<Question> { question1, question2, question3, question4, question5, question6 });
+            Questions.AddRange(questionsList);
+
+            Random random = new Random();
+            var selectedQuestions = questionsList.OrderBy(x => random.Next()).Take(3).ToList();
+            Questions = selectedQuestions;
         }
     }
 }
