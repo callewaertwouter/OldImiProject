9dd1fb2ff58c77d1b7b104bc4a84f8b041914901
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-21 15:45:07 +0200

    Update entity

--- src/Imi.Project.Api.Core/Entities/Recipe.cs
+++ src/Imi.Project.Api.Core/Entities/Recipe.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Imi.Project.Api.Core.Entities
+namespace Imi.Project.Api.Core.Entities;
+
+	public class Recipe : BaseEntity
 {
-    public class Recipe : BaseEntity
-    {
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Description { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
 
-        // Many recipes will use many ingedriënts
-        public ICollection<Ingedrient> Ingedrients { get; set; }
+    // Many recipes will use many ingedriënts
+    public ICollection<Ingedrient> Ingedrients { get; set; } = new List<Ingedrient>();
 
-        public string UserId { get; set; }
-        public User User { get; set; }
-    }
+    public string UserId { get; set; }
+    public User User { get; set; }
 }
