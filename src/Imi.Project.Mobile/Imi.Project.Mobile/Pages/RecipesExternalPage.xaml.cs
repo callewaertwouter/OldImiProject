dfdf352d3743468d3c18958bda331c5f6f273aa0
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-21 15:46:27 +0200

    Add new pages

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Pages/RecipesExternalPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Imi.Project.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Imi.Project.Mobile.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RecipesExternalPage : ContentPage
+	{
+		public RecipesExternalPage ()
+        {
+            InitializeComponent();
+
+            BindingContext = new RecipesExternalViewModel();
+            LoadRecipes();
+        }
+
+        private async void LoadRecipes()
+        {
+            var viewModel = (RecipesExternalViewModel)BindingContext;
+            await viewModel.LoadRecipesAsync();
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+        }
+    }
+}
