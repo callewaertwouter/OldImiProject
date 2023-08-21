307daf3205dd9fabed8e549acd3b8d90eb3f6138
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-21 15:46:08 +0200

    Add external api response class

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Api/RecipeApiResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Mobile.Domain.Services.Api
+{
+    public class RecipeApiResponse
+    {
+        public List<TheMealDbRecipe> Meals { get; set; }
+    }
+
+    public class TheMealDbRecipe
+    {
+        public string IdMeal { get; set; }
+        public string StrMeal { get; set; }
+        public string StrCategory { get; set; }
+        public string StrInstructions { get; set; }
+    }
+}
