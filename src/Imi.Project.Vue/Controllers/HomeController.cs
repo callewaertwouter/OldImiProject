b3ed3ace223d1d95fe299444f0fc8b024f609ed1
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-24 20:46:41 +0200

    Add login/register

--- src/Imi.Project.Vue/Controllers/HomeController.cs
+++ src/Imi.Project.Vue/Controllers/HomeController.cs
@@ -38,6 +38,16 @@ namespace Imi.Project.Vue.Controllers
             return View();
         }
 
+        public IActionResult Register() 
+        {
+            return View();
+        }
+
+        public IActionResult Login() 
+        { 
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
