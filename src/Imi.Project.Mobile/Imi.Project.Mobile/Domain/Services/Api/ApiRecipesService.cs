6d02876ed59cc5c81b89678da82d1fa9d8363249
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-27 20:34:14 +0200

    Update external API service

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Api/ApiRecipesService.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Domain/Services/Api/ApiRecipesService.cs
@@ -1,48 +1,93 @@
 ﻿using Imi.Project.Mobile.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Mobile.Domain.Services.Api
 {
-    public class ApiRecipesService : IRecipeService
-    {
-        private string baseUri = Constants.BaseUrl;
-        protected IWebApiClient _webApiClient;
-        public ApiRecipesService(IWebApiClient client)
-        {
-            _webApiClient = client;
-        }
+	public class ApiRecipesService : IRecipeService
+	{
+		private string baseUri = Constants.BaseUrl;
+		protected IWebApiClient _webApiClient;
+		private readonly IRecipeService _mockService;
 
-        public IEnumerable<Recipe> GetRecipes()
-        {
-            var recipesApi = _webApiClient.GetApiResult<IEnumerable<Recipe>>($"{baseUri}/recipes");
+		public ApiRecipesService(IWebApiClient client, 
+								 IRecipeService mockService)
+		{
+			_webApiClient = client;
+			_mockService = mockService;
+		}
 
-            var recipes = new List<Recipe>();
-            recipes.AddRange((IEnumerable<Recipe>)recipesApi);
+		public async Task<IEnumerable<Recipe>> GetRecipes()
+		{
+			try
+			{
+				var recipes = await _webApiClient.GetApiResult<IEnumerable<Recipe>>($"{baseUri}/recipes");
+				return recipes.OrderBy(recipe => recipe.Title);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return await _mockService.GetRecipes();
+			}
+		}
 
-            return recipes.OrderBy(recipe => recipe.Title);
-        }
+		public async Task<Recipe> GetRecipe(Guid id)
+		{
+			try
+			{
+				var recipe = await _webApiClient.GetApiResult<Recipe>($"{baseUri}/recipes/{id}");
+				return recipe;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return await _mockService.GetRecipe(id);
+			}
+		}
 
-        public Recipe GetRecipe(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+		public async Task<Recipe> AddRecipe(Recipe recipe)
+		{
+			try
+			{
+				var createdRecipe = await _webApiClient.PostCallApi<Recipe, Recipe>($"{baseUri}/recipes", recipe);
+				return createdRecipe;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return await _mockService.AddRecipe(recipe);
+			}
+		}
 
-        public Task<Recipe> AddRecipe(Recipe recipe)
-        {
-            throw new NotImplementedException();
-        }
+		public async Task<Recipe> UpdateRecipe(Recipe recipe)
+		{
+			try
+			{
+				var updatedRecipe = await _webApiClient.PutCallApi<Recipe, Recipe>($"{baseUri}/recipes/{recipe.Id}", recipe);
+				return updatedRecipe;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return await _mockService.UpdateRecipe(recipe);
+			}
+		}
 
-        public Task<Recipe> UpdateRecipe(Recipe recipe)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Recipe> DeleteRecipe(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public async Task<Recipe> DeleteRecipe(Guid id)
+		{
+			try
+			{
+				var deletedRecipe = await _webApiClient.DeleteCallApi<Recipe>($"{baseUri}/recipes/{id}");
+				return deletedRecipe;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return await _mockService.DeleteRecipe(id);
+			}
+		}
+	}
 }
