7e1d76dc50ba99741ee23c50a06b685d53d7d8ea
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-27 21:54:35 +0200

    Add push notification plugin

---
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Plugin/PushNotificationForUsers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Notifications;
+
+namespace Imi.Project.Mobile.Plugin
+{
+	public class PushNotificationForUsers
+	{
+		public void PushNotification(string recipeName)
+		{
+			// Code voor de push notificatie
+			var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+			var textElements = toastXml.GetElementsByTagName("text");
+			textElements[0].AppendChild(toastXml.CreateTextNode($"New Recipe: {recipeName}"));
+
+			// Timer voor de notificatie
+			var toast = new ScheduledToastNotification(toastXml, DateTime.Now.AddSeconds(5));
+			ToastNotificationManager.CreateToastNotifier().AddToSchedule(toast);
+		}
+	}
+}
