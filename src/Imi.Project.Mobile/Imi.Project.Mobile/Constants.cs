a67e4f5b5c65cc428ee1a57e82adcc4e5913e838
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-27 20:35:28 +0200

    Update constants

--- src/Imi.Project.Mobile/Imi.Project.Mobile/Constants.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile/Constants.cs
@@ -2,6 +2,7 @@
 {
     public class Constants
     {
-        public const string BaseUrl = "https://192.168.0.204:5001";
+        //public const string BaseUrl = "https://192.168.0.204:5001";
+        public const string BaseUrl = "https://localhost:5001";
     }
 }
