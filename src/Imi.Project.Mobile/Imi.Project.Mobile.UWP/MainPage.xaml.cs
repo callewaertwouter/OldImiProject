0912a98bd708dedfc8847bfc9fea5f4f83cee6dc
woutercallewaert <wouter.callewaert@student.howest.be>
2023-08-27 20:35:08 +0200

    Remove commented code

--- src/Imi.Project.Mobile/Imi.Project.Mobile.UWP/MainPage.xaml.cs
+++ src/Imi.Project.Mobile/Imi.Project.Mobile.UWP/MainPage.xaml.cs
@@ -1,22 +1,12 @@
-﻿using Windows.ApplicationModel.Activation;
-using Xamarin.Forms;
-
-namespace Imi.Project.Mobile.UWP
+﻿namespace Imi.Project.Mobile.UWP
 {
-    public sealed partial class MainPage
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
+	public sealed partial class MainPage
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
 
-            LoadApplication(new Imi.Project.Mobile.App());
-        }
-
-        //protected override void OnLaunched(LaunchActivatedEventArgs e)
-        //{
-        //    SkiaSharp.Views.UWP.SKCanvasViewRenderer.Init();
-
-        //    base.OnLaunched(e);
-        //}
-    }
+			LoadApplication(new Mobile.App());
+		}
+	}
 }
